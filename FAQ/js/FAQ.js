1f93340a930b9f63086b8a1acb55e16a33c9cbcc
Yoojin Lee <yoojinLee.dev@gmail.com>
2020-10-27 17:59:34 +0900

    feat:Add event to h2, a tag

--- FAQ/js/FAQ.js
+++ FAQ/js/FAQ.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
 /*Add event a tag*/
+  $("h2").click(function(){
+    alert("event execute")
+  })
+  $("a").click(function(){
+    alert("event execute")
+  })
   $("a").mouseenter(function(){
     $(this).addClass("strongBold")
   })
-  
   $("a").mouseleave(function(){
     $(this).removeClass()
   })
-  
 })
